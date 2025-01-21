8fe86b86d40a40cfc52aba4311846aa26e3d239d
coiilee <dakyung96@gmail.com>
2025-01-21 16:55:17 +0900

    passwordencoder

--- src/main/java/com/kht/ecommerce/ecommerce_application/service/UserServiceImpl.java
+++ src/main/java/com/kht/ecommerce/ecommerce_application/service/UserServiceImpl.java
@@ -21,6 +21,8 @@ UserMapper userMapper;
     public void insertUser(User user){
         userMapper.insertUser(user); //DB에서 가져온 값을 return해서 반환할 필요 X
         //DB에 저장을 하는 것이지 데이터베이스에서 꺼내오는 것이 아니기 때문에 return 아닌 void 사용
+
+
     }
 
     @Override
