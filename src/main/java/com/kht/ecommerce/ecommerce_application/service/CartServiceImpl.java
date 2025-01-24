790a6f8b5e7e9d23cee7fe2c55cff5543de589c1
coiilee <dakyung96@gmail.com>
2025-01-24 11:04:09 +0900

    -

--- src/main/java/com/kht/ecommerce/ecommerce_application/service/CartServiceImpl.java
+++ src/main/java/com/kht/ecommerce/ecommerce_application/service/CartServiceImpl.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class CartServiceImpl implements CartService {
     @Autowired
     CartMapper cartMapper;
+
     @Override
     public List<Cart> getCartByUserId(int userId) {
         return cartMapper.getCartByUserId(userId);
